cbb3ab3e6ca8698fc01d3741fa3b7c2045359af9
Frankie Sanjana <francesca.sanjana@gmail.com>
2023-10-25 17:13:53 +0800

    Build: Add final section of sale price predictor

--- app_pages/page_sale_price_predictor.py
+++ app_pages/page_sale_price_predictor.py
@@ -34,14 +34,97 @@ def page_sale_price_predictor_body():
         f"prediction, and display the predicted sale price for each house:"
     )
     df_inherited_filtered = df_inherited_houses.filter(house_features)
-    sale_price_prediction = sale_price_pipeline.predict(df_inherited_filtered)
+    sale_price_prediction = np.round(sale_price_pipeline.predict(df_inherited_filtered), 0)
     df_inherited_filtered["Predicted Sale Price"] = sale_price_prediction
 
     st.write(df_inherited_filtered.head())
 
-    summed_sale_price = round(df_inherited_filtered["Predicted Sale Price"].sum(), 2)
+    summed_sale_price = "${:,.0f}".format(df_inherited_filtered["Predicted Sale Price"].sum())
 
     st.success(
         f"* The total predicted sale price for the four inherited houses "
-        f"is ${summed_sale_price}."
+        f"is {summed_sale_price}."
     )
+    st.write("---")
+    # generate live data
+    # predict the sale price of any other house in Ames, Iowa
+    st.info("### Predicting sales price of any other house in Ames, Iowa (BR2, part 2)")
+    st.info(
+       f"* **BR2** - The client is interested in predicting the house sale prices "
+       f"from her 4 inherited houses, and any other house in Ames, Iowa."
+    )
+    st.write(
+        f"* To predict the sale price of another house, please enter values for the "
+        f"following attributes of the house and click the 'Predict Sale Price' button:"
+    )
+    
+    X_live = draw_input_widgets()
+
+    # predict on live data
+    if st.button("Predict Sale Price"):
+        price_prediction = predict_sale_price(X_live, house_features, sale_price_pipeline)
+    st.write("---")
+
+def draw_input_widgets():
+    """
+    Defines the input widgets for the user to enter attributes to predict
+    the sale price of any house in Ames, Iowa
+    """
+    # load dataset
+    df = load_housing_data()
+    # stipulate min and max percentage values for numerical vars
+    percentage_min, percentage_max = 0.2, 3.0
+    # create input widgets for the four most relevant features
+    col1, col2 = st.beta_columns(2)
+    col3, col4 = st.beta_columns(2)
+    # these features are used to feed the ML pipeline
+    # create an empty DataFrame, which will be the live data
+    X_live = pd.DataFrame([], index=[0])
+    # we now draw the widget based on the variable type
+    # (numerical or categorical), and set initial values
+    with col1:
+        feature = "TotalBsmtSF"
+        st_widget = st.number_input(
+            label = feature,
+            min_value = df[feature].min()*percentage_min,
+            max_value = df[feature].max()*percentage_max,
+            value = df[feature].median()
+        )
+    X_live[feature] = st_widget
+
+    with col2:
+        feature = "YearBuilt"
+        # round the median to the nearest whole year
+        median_value = round(df[feature].median())
+        st_widget = st.number_input(
+            label = feature,
+            min_value = df[feature].min(),
+            # set max to 2100, since by then our predictor will likely be obsolete
+            max_value = 2100,
+            value = median_value,
+            # restrict input values to whole numbers only
+            step = 1
+        )
+    X_live[feature] = st_widget
+
+    with col3:
+        feature = "2ndFlrSF"
+        st_widget = st.number_input(
+            label = feature,
+            min_value = df[feature].min()*percentage_min,
+            max_value = df[feature].max()*percentage_max,
+            value = df[feature].median()
+        )
+    X_live[feature] = st_widget
+
+    with col4:
+        feature = "LotArea"
+        st_widget = st.number_input(
+            label = feature,
+            min_value = df[feature].min()*percentage_min,
+            max_value = df[feature].max()*percentage_max,
+            value = df[feature].median()
+        )
+    X_live[feature] = st_widget
+
+    return X_live
--- src/machine_learning/predictive_analysis_ui.py
+++ src/machine_learning/predictive_analysis_ui.py
@@ -6,12 +6,18 @@ def predict_sale_price(X_live, house_features, sale_price_pipeline):
     # from the live data, subset features related to this pipeline
     X_live_sale_price = X_live.filter(house_features)
 
-    # apply the pipeline to the live data and predict sale price
-    # predict
+    # apply the pipeline to the live data and predict the sale price
     sale_price_prediction = sale_price_pipeline.predict(X_live_sale_price)
+    
+    # format the predicted price to round and show dollar sign
+    # we round to the nearest whole dollar
+    sale_price_prediction = float(sale_price_prediction)
+    predicted_sale_price = "${:,.0f}".format(sale_price_prediction)
 
-       # create a statement to display the results
+    # create a statement to display the results
     statement = (
         f'The predicted sale price for the house based on the '
-        f'provided feature values is: **{sale_price_prediction}**.'
+        f'provided attribute values is: **{predicted_sale_price}**.'
     )
+
+    st.success(statement)
